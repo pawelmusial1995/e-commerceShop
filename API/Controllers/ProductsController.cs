ca486a2f2462633ba7c43fcc3a8ff0326206d38b
Pawel <pawelmusial1995@gmail.com>
2022-05-15 13:32:57 +0200

    Eager loading of product properties

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -35,5 +35,19 @@ namespace API.Controllers
             return Ok(_productRepository.GetProductByIdAsync(id));
         }
 
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() 
+        {
+            return Ok(await _productRepository.GetProductBrandsAsync());
+        } 
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() 
+        {
+            return Ok(await _productRepository.GetProductTypesAsync());
+        }
+
+       
+
     }
 }
